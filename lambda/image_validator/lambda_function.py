9a95926ad19ac8a129dd83ff8c33753742e24c8c
Sachit Anand <sachitanand@Sachits-MacBook-Pro-2.local>
2026-03-29 13:47:25 -0400

    Implemented lambda and deploy

--- lambda/image_validator/lambda_function.py
+++ lambda/image_validator/lambda_function.py
@@ -59,4 +59,23 @@ def lambda_handler(event, context):
     #         - print the [INVALID] message: print(f"[INVALID] {key} is not a valid image type")
     #         - raise ValueError to trigger DLQ
 
+    for record in event['Records']:
+        sns_message = json.loads(record['Sns']['Message'])
+
+        for s3_record in sns_message['Records']:
+            bucket = s3_record['s3']['bucket']['name']
+            key    = s3_record['s3']['object']['key']
+
+            if is_valid_image(key):
+                print(f"[VALID] {key} is a valid image file")
+                filename = key.split('/')[-1]
+                s3.copy_object(
+                    Bucket=bucket,
+                    Key=f"processed/valid/{filename}",
+                    CopySource={'Bucket': bucket, 'Key': key}
+                )
+            else:
+                print(f"[INVALID] {key} is not a valid image type")
+                raise ValueError(f"{key} is not a valid image type")
+
     return {'statusCode': 200, 'body': 'validation complete'}
